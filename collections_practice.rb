81b652581f0605befde5bd193f95bd8d81b15686
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-11-14 22:12:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
+numbers=[3,5,2]
+def sort_array_asc(numbers)
+numbers.sort 
+end 
+numbersdesc=[5,7,9,112]
 
+def sort_array_desc(numbersdesc)
+  numbersdesc.sort.reverse
+end 
+
+
+def sort_array_char_count(animals)
+  animals=["cat", "dogs", "Horses"]
+  animals = animals.sort_by {|x| x.length}
+end 
+
+def swap_elements(array)
+  array = ["blake","ashley","scott"]
+
+array[0], array[1], array[2] = array[0], array[2], array[1]
+end 
+
+def reverse_array(integers)
+  integers=[12,4,35]
+  integers.reverse 
+end 
+
+  def kesha_maker(array)
+  dollars = []
+  array.each do |word|
+    word[2] = "$"
+    dollars << word
+  end
+end
+ 
